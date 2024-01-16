ca04c294fc0834e85632e5c607ce445c1743f9a8
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2024-01-16 20:51:58 +0900

    feat: initialize_agent와 OPENAI_FUNCTIONS 를 이용한 tool 선택

--- code-interpreter/main.py
+++ code-interpreter/main.py
@@ -1,37 +1,74 @@
-from dotenv import load_dotenv
-from langchain_experimental.agents.agent_toolkits import create_python_agent
-from langchain_experimental.tools import PythonREPLTool
+from langchain.agents import AgentType, initialize_agent
+from langchain.tools import Tool
 from langchain_openai.chat_models import ChatOpenAI
-from langchain.agents import AgentType
-
-load_dotenv()
+from langchain_experimental.tools import PythonREPLTool
+from langchain_experimental.agents.agent_toolkits import (
+    create_csv_agent,
+    create_python_agent,
+)
 
 
 def main():
     print("start...")
 
-    # model="gpt-4" 로 변경하면 훨씬 안정적으로 코드를 생성함
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
-    tool = PythonREPLTool()  # only one tool, PythonREPLTool
+    # model="gpt-4" 로 변경하면 훨씬 안정적으로 응답을 생성함
+    llm = ChatOpenAI(temperature=0, model="gpt-4")
+
     python_agent_executor = create_python_agent(
         llm=llm,
-        tool=tool,
+        tool=PythonREPLTool(),
         agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         verbose=True,
     )
+    csv_agent_executor = create_csv_agent(
+        llm=llm,
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        path="episode_info.csv",
+        verbose=True,
+    )
 
-    print(f"{tool=}")
+    tools = [
+        Tool(
+            name="python_agent",
+            func=python_agent_executor.invoke,
+            description="""
+                useful when you need to transform natural language and write from it python and execute the python code,
+                returning the results of the code execution,
+                TIPS: DO NOT SEND PYTHON CODE TO THIS TOOL DIRECTLY.
+                """,
+        ),
+        Tool(
+            name="csv_agent",
+            func=csv_agent_executor.invoke,
+            description="""
+                useful when you need to answer question over episode_info.csv file,
+                takes an input the entire question and returns the answer after running pandas calculations
+                """,
+        ),
+    ]
+
+    # 일반 도구를 선택하는 prompt는 OpenAI Function Interface로 대체할 수 있음
+    # 현재 initialize_agent는 deprecated
+    grand_agent = initialize_agent(
+        tools=tools,
+        llm=llm,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        verbose=True,
+    )
+
+    # [ ] question for python_agent
     question_message = """
-    generate and save in current working directory 15 QRcodes that point to url = "www.udemy.com/course/langchain". 
-    TIPS: ALL Python libraries are already installed.
-"""
+    generate and save in current working directory 15 QRcodes that point to www.udemy.com/course/langchain.
+    you have qrcode package installed already.
     """
-    필요한 패키지를 미리 다 설치해줘야 함.
-    로컬 터미널을 실행하는 Tool은 없기 때문에 !!
-    이후, 프롬프트로 라이브러리를 설치할 필요가 없다는 사실을 instruct 
-        ( TIPS: ALL Python libraries are already installed. )
+
+    # [v] question for csv_agent
+    question_message = """
+    print seasons ascending order of the number of episodes they have.
+    TIPS: Run the code script at once if possible.
     """
-    python_agent_executor.run(question_message)
+    result = grand_agent.invoke(question_message)
+    print(f"{result=}")
 
 
 if __name__ == "__main__":
